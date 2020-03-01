6ef7d687ba9cffc50fd865fef25c31aa9d58861b
bartoszszczepanski <szczepanski@student.agh.edu.pl>
2020-03-01 11:10:47 +0100

    graphics draft

---
+++ src/main/java/pl/edu/agh/hangman/Graphics.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.hangman;
+
+public class Graphics {
+
+    private
+
+    public String getAnimation(int triesLeft) {
+    }
+
+    public String getWord(Object guessed) {
+    }
+}
